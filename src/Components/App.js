6edd79788d537ffe6caf500383bbf080255bafd0
Isadora Ávila <isadorgravila@gmail.com>
2022-06-28 10:23:12 -0300

    Importação de bibliotecas + criação das telas entrar e cadastro

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,18 @@
 import "../Assets/reset.css";
 import "../Assets/style.css";
+import TelaEntrar from "./TelaEntrar";
+import TelaCadastro from "./TelaCadastro";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
     return (
-        <div>Início do projeto</div>
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<TelaEntrar />} />
+                    <Route path="/cadastro" element={<TelaCadastro />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
---
+++ src/Components/TelaEntrar.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
+
+export default function TelaEntrar() {
+    return (
+        <div>
+            Entrar;
+        </div>
+    );
+}
